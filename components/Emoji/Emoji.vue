bcc0d09c0acd55853847859254f28479c10cc015
Cauchy03 <1481856337@qq.com>
2023-07-01 10:02:21 +0800

    :construction: feat: support sending Emoji

---
+++ components/Emoji/Emoji.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="emoji">
+		<view class="emoji-line" v-for="(line, index) in emojis" :key="index">
+			<view class="emoji-item" v-for="(item, index) in line" :key="index" @tap="clickEmoji(item)">
+				{{item}}
+			</view>
+		</view>
+	</view>
+
+</template>
+
+<script setup>
+	import {
+		reactive,
+		ref
+	} from "vue";
+	let emojis = ref([
+		['😀', '😁', '😂', '🤣', '😃', '😄', '😅', '😆'],
+		['😉', '😊', '😋', '😎', '😍', '😘', '😗', '😙'],
+		['😚', '🙂', '🤗', '🤔', '😐', '😑', '😶', '🙄'],
+		['😏', '😣', '😥', '😮', '🤐', '😯', '😪', '😫']
+	])
+	let emit = defineEmits(['sendEmoji'])
+	const clickEmoji = emoji => {
+		emit('sendEmoji', emoji)
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
